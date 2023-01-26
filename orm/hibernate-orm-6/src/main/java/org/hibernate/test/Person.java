2ea1f344ce113b50ea61cc626fc8f1adaba112d2
Bart Vansegbroeck <b.vansegbroeck@ccv.eu>
2023-01-26 10:01:36 +0100

    Created failing test case in 6.1.6 and identical working test case in 5.6.14

--- orm/hibernate-orm-6/src/main/java/org/hibernate/test/Person.java
+++ orm/hibernate-orm-6/src/main/java/org/hibernate/test/Person.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
